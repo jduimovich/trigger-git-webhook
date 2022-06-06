f011b41f2b769528c4a5b380dae509ffaf082321
John Duimovich <jduimovich@gmail.com>
2022-06-06 08:42:39 -0400

    run-from-any

--- trigger-webhook.sh
+++ trigger-webhook.sh
@@ -1,5 +1,7 @@
  #!/bin/bash
  # trigger-webhook  
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 URL=http://$1
 TAG=$2 
 if [ -z "$TAG" ]
@@ -10,7 +12,8 @@ echo "$0 $URL $TAG "
 
 FILE=$(mktemp) 
 PATCH=".head_commit.id=\"$TAG\""   
-jq "$PATCH" payload.json > $FILE   
-curl --silent -d @$FILE -H 'Content-Type: application/json'  $URL | jq
+jq "$PATCH" SCRIPTDIR/payload.json > $FILE   
+#
+curl.exe --silent --insecure -d @$FILE -H 'Content-Type: application/json'  $URL | jq
  
  
